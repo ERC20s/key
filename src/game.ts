3477969ab85723278a56fcccde9679e1866095f5
ERC20s <55540259+ERC20s@users.noreply.github.com>
2021-11-12 11:19:33 +0000

    .

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,24 @@
+
+
+
+
+
 const wall = new Entity();
 engine.addEntity(wall);
-wall.addComponent(new GLTFShape("models/walls2.glb"));
+wall.addComponent(new GLTFShape("models/walls3.glb"));
 wall.addComponent(new Transform({ position: new Vector3(0, 0, 0) }));
+
+const key = new Entity();
+engine.addEntity(key);
+// key.addComponent(audioSource)
+key.addComponent(new GLTFShape("models/key.glb"));
+key.addComponent(new Transform({ position: new Vector3(0, 0, 0)}));
+
+key.addComponent(
+  new OnPointerDown(() => {
+    openExternalURL("https://medium.com/we-are-talisman")
+  },
+    { hoverText: "We are talisman!",
+    distance: 130, }
+)
+)
